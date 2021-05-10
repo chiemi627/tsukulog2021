71c5f9c3f8ecc2961059f98094ec095ebe7bee47
Chiemi Watanabe <chiemi@a.tsukuba-tech.ac.jp>
2021-05-10 18:37:37 +0900

    tabelog versionに変更

--- server.js
+++ server.js
@@ -1,40 +1,29 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const basicAuth = require('./basicAuth');
 const app = express();
 app.set("view engine", "ejs");
 const fs = require("fs");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use(basicAuth);
 module.exports = app;
 
 
 // we've started you off with Express,
 // but feel free to use whatever libs or frameworks you'd like through `package.json`.
 
-// http://expressjs.com/en/starter/static-files.html
 app.use(express.static("public"));
 
 // init sqlite db
-const dbFile = "tsukutter.db";
-//const dbFile = "https://cdn.glitch.com/9f780427-2d67-43db-8909-7b1264c5c820%2Ftsukutter.db?v=1618911440340";
+const dbFile = "tabelog.db";
 const exists = fs.existsSync(dbFile);
 const sqlite3 = require("sqlite3").verbose();
 const db = new sqlite3.Database(dbFile);
 
-app.get("/", (request, response) => {
-    const query = "SELECT display_name,content from tweet";
-    db.all(query, (err, rows) => {
-        response.render("index.ejs", { results: rows });
-    });
-});
 
-
-app.get("/query", (request, response) => {
-    const query = "SELECT display_name,content from tweet where display_name = ?";
-    db.all(query, request.query.display_name, (err, rows) => {
-        response.render("index.ejs", { results: rows });
+app.get("/item/:id", (request, response) => {
+    const query = "SELECT name,genre,address,closed,rate FROM item where shopID=?";
+    db.all(query, request.params.id, (err, rows) => {
+        response.render("tabelog.ejs", rows[0]);
     });
 });
 
